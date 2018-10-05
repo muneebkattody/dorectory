adb3a6855c31d0534f792a9d00472d87cfb978df
muneebkattody <muneebkattody@users.noreply.github.com>
2018-10-05 23:21:35 +0530

    base structure

---
+++ server/newdata.php
@@ -0,0 +1,28 @@
+<?php
+    header('Access-Control-Allow-Origin: *');
+// error_reporting(0);
+$servername = "localhost";
+$username = "*****";
+$password = "*****";
+$dbname = "*****";
+// Create connection
+$new = new mysqli($servername, $username, $password,$dbname);
+
+// Check connection
+ if ($new->connect_error) {
+     die("Connection failed: " . $conn->connect_error);
+ }
+
+    $name = $_POST['name'];
+    $dept = $_POST['dept'];
+    $place = $_POST['place'];
+    $hosp = $_POST['hosp'];
+
+
+             $query = "INSERT INTO input(name,department,hospital,place) values('".$name."','".$dept."','".$hosp."','".$place."')";
+             if($new->query($query))    {
+                 echo "Data Uploaded Successfully!";    
+              }
+
+             
+    ?>
